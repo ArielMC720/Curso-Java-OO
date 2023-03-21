d51c8afb5da53aa49ae7be36fccf2bcd52140d0e
ARIEL_MC <arieldbod@gmail.com>
2023-03-21 00:57:08 -0400

    adicion del propietario al programa

---
+++ DPropietario.java
@@ -0,0 +1,8 @@
+public class DPropietario {
+    public static void main(String[] args) {
+        Propietario propietario = new Propietario();
+        propietario.Leer();
+        propietario.Imprimir();
+    }
+
+}
